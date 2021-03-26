5448fc19a513100ed6f33d2f55833bcd48e155c2
“shafikulislam067@gmail.comgit config --global user.name “shafikul islam <shafikulislam067@gmail.comgit config --global user.email shafikulislam067@gmail.com>
2021-03-26 12:20:38 +0600

    updat

---
+++ index.js
@@ -0,0 +1,98 @@
+const express = require('express');
+
+const bodyParser = require('body-parser');
+
+const ObjectId = require('mongodb').ObjectId;
+
+const password = 'hCEtGqLD0rVlKyZz'
+
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://mydbUser:hCEtGqLD0rVlKyZz@cluster0.ywwhy.mongodb.net/organicdb?retryWrites=true&w=majority";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+
+
+
+app.get('/' ,( req ,res ) =>{
+
+    res.sendFile(__dirname + '/index.html');
+
+})
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const productCollection = client.db("organicdb").collection("products");
+  app.get('/products' , (req ,res ) =>{
+    productCollection.find({})
+    .toArray((err , documents  )=>{
+      res.send(documents)
+    })
+
+  })
+
+
+
+
+ app.post("/addProduct",(req ,res) => {
+   const product = req.body;
+   productCollection.insertOne(product)
+   .then(result => {
+     console.log('data added success')
+     res.send('success')
+   })
+ })
+
+
+ app.get('/product/:id' , (req ,res ) =>{
+   productCollection.find({_id:ObjectId(req.params.id)})
+   .toArray((err ,documents)=>{
+     res.send(documents[0]);
+   })
+
+ })
+
+ app.patch('/update/:id', (req, res)=>{
+   console.log(req.body.price);
+   productCollection.updateOne({_id:ObjectId(req.params.id)},
+   {
+     $set:{price: req.body.price , quantity: req.body.quantity}
+   })
+
+   .then(result =>{
+     console.log(result)
+   })
+ })
+
+
+
+//  app.patch('/update/:id', (req ,res)=>{
+//    console.log(req.body.price);
+//   productCollection.updateOne({_id:ObjectId(req.params.id)},
+//   {
+//     $set: {price:req.body.price, quantity:req.body.quantity}
+//   })
+//   .then(result=>{
+//     console.log(result)
+//   })
+
+
+//  })
+
+ app.delete('/delete/:id',(req ,res) =>{
+   productCollection.deleteOne({_id:ObjectId(req.params.id)})
+   .then((result) =>{
+     console.log(result)
+   })
+ 
+})
+
+});
+
+
+
+app.listen(3000);
